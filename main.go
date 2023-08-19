d4d2b5d4a64b88edfff08dd8ce243316da96950a
umiatiyah <umiatiyah97@gmail.com>
2023-08-20 06:21:01 +0700

    set gin mode as environment

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"log"
 	"os"
 	"portfolio-go/routing"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	if os.Getenv("ENV") != "development" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
 	r := routing.InitializeRoute()
 	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
