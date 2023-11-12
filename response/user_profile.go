1089fb75e12a9b3320cc53ada79434700b4db728
umiatiyah <umiatiyah97@gmail.com>
2023-11-12 23:20:32 +0700

    add new response to show current job and github link profile user

--- response/user_profile.go
+++ response/user_profile.go
@@ -5,10 +5,16 @@ import (
 )
 
 type UserProfileResponse struct {
-	UserProfile model.UserProfile   `json:"user_profile"`
+	UserProfile ProfileResponse     `json:"user_profile"`
 	SocialMedia []model.SocialMedia `json:"social_media"`
 	Hobby       []model.Hobby       `json:"hobby"`
 	Experience  []model.Experience  `json:"experience"`
 	Education   []model.Education   `json:"education"`
 	Contact     []model.Contact     `json:"contact"`
+}
+
+type ProfileResponse struct {
+	model.UserProfile
+	CurrentJob string `json:"current_job"`
+	GithubLink string `json:"github_link"`
 }
