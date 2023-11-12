4e21b13ed1b9850612c6c8b396941a874d810e25
umiatiyah <umiatiyah97@gmail.com>
2023-11-12 23:17:52 +0700

    allow cors origin, method, and header

--- routing/routes.go
+++ routing/routes.go
@@ -4,12 +4,20 @@ import (
 	"portfolio-go/handler"
 	"portfolio-go/middleware"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeRoute() *gin.Engine {
 	r := gin.Default()
 
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders: []string{"*"},
+	}))
+
+	r.Use(cors.Default())
 	r.GET("/health", handler.CheckHealth)
 
 	auth := r.Group("/auth")
